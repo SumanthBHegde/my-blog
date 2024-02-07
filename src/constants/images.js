d5fc36807442067c4ab9e2100c97678896d2f2a6
SumanthBHegde <sumanthbhegde12@gmail.com>
2024-02-07 20:27:44 +0530

    Added article card

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,8 +1,13 @@
 import logo from "../assets/logo.png";
-import HeroImage from "../assets/logo.png";
+import HeroImage from "../assets/Hero.jpg";
+import Post1Image from "../assets/posts/post1.jpg"
+import PostProfileImage from "../assets/posts/profile-pic.svg"
+
 const images = {
     logo,
     HeroImage,
+    Post1Image,
+    PostProfileImage,
 };
 
 export default images;
--- src/pages/home/container/Hero.jsx
+++ src/pages/home/container/Hero.jsx
@@ -5,13 +5,13 @@ const Hero = () => {
   return (
     <section className='container mx-auto flex flex-col px-5 py-5 lg:flex-row'>
       <div className='mt-10 lg:w-1/2'>
-        <h1 className='font-roboto text-3xl text-center font-bold text-dark-soft md:text-5xl lg:text-lest lg:max-w-[540px]'>
+        <h1 className='font-roboto text-3xl text-center font-bold text-dark-soft md:text-5xl lg:text-4xl xl:text-5xl lg:text-lest lg:max-w-[540px]'>
             Read any intresting stories of your friends
         </h1>
-        <p className="text-dark-light mt-4 text-center md:text-xl lg:text-left">
+        <p className="text-dark-light mt-4 text-center md:text-xl lg:text-left lg:text-base xl:text-xl">
             ......
         </p>
-        <div className="flex flex-col gap-y-2.5 mt-10 relative">
+        <div className="flex flex-col gap-y-2.5 mt-10 relative lg:mt-6 xl:mt-10">
             <div className="relative">
                 <FiSearch className='absolute left-3 top-1/2 -translate-y-1/2 w-6 h-6 text-[#959EAD]'/>
                 <input className="placeholder:font-bold font-semibold text-dark-soft placeholder:text-[#959EAD] rounded-lg pl-12 pr-3 w-full py-3 focus:outline-none shadow-[rgba(13,_38,_76,_0.19)_0px_9x_20px] md:py-4 " type="text" placeholder="Search Story"/>
@@ -19,16 +19,16 @@ const Hero = () => {
         <button className="w-full bg-primary text-white font-semibold rounded-lg px-5 py-3 md:absolute md:right-2 md:top-1/2 md:-translate-y-1/2 md:w-fit md:py-2">Serach</button>  
         </div>
         <div className="flex mt-4 flex-col lg:flex-row lg:items-start lg:flex-nowrap lg:gap-x-4 lg:mt-7">
-            <span className="text-dark-light font-semibold italic mt-2 lg:mt-4">Popular Stories</span>
+            <span className="text-dark-light font-semibold italic mt-2 lg:mt-4 lg:text-sm xl:text-base">Popular Stories</span>
             <ul className="flex flex-wrap gap-x-2.5 gap-y-2.5 mt-3">
-                <li className="rounded-lg bg-primary bg-opacity-10 px-3 py-1.5 text-primary font-semibold">Treking</li>
-                <li className="rounded-lg bg-primary bg-opacity-10 px-3 py-1.5 text-primary font-semibold">Chemwiz</li>
-                <li className="rounded-lg bg-primary bg-opacity-10 px-3 py-1.5 text-primary font-semibold">Field Visit</li>
+                <li className="rounded-lg bg-primary bg-opacity-10 px-3 py-1.5 text-primary font-semibold lg:text-sm xl:text-base">Treking</li>
+                <li className="rounded-lg bg-primary bg-opacity-10 px-3 py-1.5 text-primary font-semibold lg:text-sm xl:text-base">Chemwiz</li>
+                <li className="rounded-lg bg-primary bg-opacity-10 px-3 py-1.5 text-primary font-semibold lg:text-sm xl:text-base">Field Visit</li>
             </ul>
         </div>
       </div>
       <div className='hidden lg:block lg:1/2'>
-        <img className="w-full" src="{images.HeroImage}" alt="article" />
+        <img className="w-3/4" src={images.HeroImage} alt="article" />
       </div>
     </section>
   )
